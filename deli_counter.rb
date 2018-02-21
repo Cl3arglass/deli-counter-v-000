7ba4b82e977acea0c1ec2528fc58acde5b448ab5
Holden Altaffer <haltaffer@gmail.com>
2018-02-21 22:49:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,6 @@ def line(katz_deli)
   else
     result = "The line is currently:"
     katz_deli.each_with_index{|name, index| result = result + " #{index + 1}. #{name}"}
+    puts result
   end
 end
